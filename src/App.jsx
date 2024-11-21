8b8ef0c0ae4d32805c49dc06c2b295612b348a29
Kan <ssitinan@gmail.com>
2024-11-21 21:09:59 +0700

    What Web Start

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import DataHardware from "./pages/DataHardware";
-import GraphHardware from "./pages/GraphHardware";
+// import GraphHardware from "./pages/GraphHardware";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -16,7 +16,7 @@ const App = () => {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/DataHardware" element={<DataHardware />} />
-        <Route path="/GraphHardware" element={<GraphHardware />} />
+        {/* <Route path="/GraphHardware" element={<GraphHardware />} /> */}
       </Route>
     )
   );
